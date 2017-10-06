6cdcdd962d9575a58d772577aeef0ec1693af9c9
Clavos <clabaq@gmail.com>
2017-10-06 15:00:19 +0200

    Modification du docker compose

--- app/index.php
+++ app/index.php
@@ -1,20 +1,7 @@
-<?php
- function validate(){
-	 echo "vous êtes connecté"
- }
-?>
-
 <html>
 	<head>
 		<title>Mon application PHP</title>
 	</head>
 	<body>
-		<form method="POST">
-			Login : <br>
-			<input type="text"/><br>
-			Mot de passe : <br>
-			<input type="mdp"/><br>
-			<button onClick="validate">Valider</button>
-		</form>
 	</body>
 </html>
